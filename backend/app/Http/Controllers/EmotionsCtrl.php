d7d87ac8be68c667cf97feba7b3cd85f1c547ef0
Camilo H <camilohamonserna@gmail.com>
2021-06-23 11:38:43 -0500

    Generate and Comunication on Emotions

--- backend/app/Http/Controllers/EmotionsCtrl.php
+++ backend/app/Http/Controllers/EmotionsCtrl.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Emotions;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use function PHPSTORM_META\type;
 
 class EmotionsCtrl extends Controller
 {
@@ -29,9 +32,44 @@ class EmotionsCtrl extends Controller
             if (is_null($emotions)) {
                 return response()->json(['message' => 'Not Found'], 404);
             }
-            return response()->json($emotions, 200);
+            $separate = explode('. ', $emotions->description);
+
+            $generate = $this->organizeValues($separate[0]);
+
+            $comunication = $this->organizeValues($separate[1]);
+     
+            $emotion = [
+                'id' => $emotions->id,
+                'name' => $emotions->name,
+                'generate' => [
+                    'title' => $generate[0],
+                    'description' => $generate[1]
+                ],
+                'comunication' => [
+                    'title' => $comunication[0],
+                    'description' => $comunication[1]
+                ]
+            ];
+            return response()->json($emotion, 200);
         } catch (Exception $e) {
+            echo $e;
             return response()->json(['message' => 'Internal Server Error'], 500);
         }
+    }
+
+    private function addCharacter($text, $character)
+    {
+        return Str::finish($text, $character);
+    }
+
+    private function organizeValues($array)
+    {
+        $separate = explode('? ', $array);
+
+        $title = $this->addCharacter($separate[0], '?');
+        $description = $this->addCharacter($separate[1], '.');
+        $res = [$title, $description];
+
+        return $res;
     }
 }
