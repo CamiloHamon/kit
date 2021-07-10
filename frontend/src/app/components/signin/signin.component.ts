4044b0a572268a06daa15937af83b609d0cec150
Camilo H <camilohamonserna@gmail.com>
2021-07-10 16:44:13 -0500

    Login mejoras

--- frontend/src/app/components/signin/signin.component.ts
+++ frontend/src/app/components/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { User } from 'src/app/classes/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,20 +12,53 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router:Router) { }
-  email = '';
-  password = '';
+  public form: FormGroup;
+  public email = '';
+  public password = '';
+  public error = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.buildForm();
   }
 
-  signIn() {
+  public ngOnInit() { }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  public getError(controlName: string): string {
+    let message = '';
+    const control = this.form.get(controlName);
+    if (control?.dirty || control?.touched) {
+      if (control?.errors?.required != null) {
+        message = 'Este campo es obligatorio'
+      } else if (control?.errors?.email != null) {
+        message = 'Debe ser un correo valido';
+      } else if (control?.hasError('minlength')) {
+        const minLength = control?.errors?.minlength.requiredLength;
+        message = `Este campo debe tener minimo ${minLength} caracteres`;
+      }
+    }
+    return message;
+  }
+
+  public signIn() {
+    this.email = this.form.get('email')?.value;
+    this.password = this.form.get('password')?.value;
     const user = new User(this.email, this.password);
     this.authService.signIn(user)
-      .subscribe(res => { 
+      .subscribe(res => {
+        const user = res.user;
+        localStorage.setItem('user', user.email);
         localStorage.setItem('token', res.token);
         this.router.navigate(['/home']);
-       }, err => { console.log(err) })
+      }, err => {
+        this.error = true;
+      })
   }
 
 }
