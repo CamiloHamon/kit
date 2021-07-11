9eaba8485b029fa916a897fdcb594862f90fc355
Camilo H <camilohamonserna@gmail.com>
2021-07-11 15:19:13 -0500

    Header movil

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { ModalDismissReasons, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -10,44 +9,24 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
   changeBg = false;
-  closeResult = '';
-  active = 0;
+  show = false;
 
-  constructor(private router: Router, public authService: AuthService, private modal: NgbModal, config: NgbModalConfig) {
-    config.backdrop = 'static';
-    config.keyboard = false;
+  constructor(private router: Router, public authService: AuthService) {
     router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
-        if (val.url === '/login') this.changeBg = true;
+        if (val.url !== '/') this.changeBg = true;
         else this.changeBg = false;
       }
     })
   }
 
+  openMenu(){
+    this.show = true;
+  }
+
+  closeMenu(){
+    this.show = false;
+  }
+
   ngOnInit() { }
-
-  open(content: any) {
-    this.modal.open(content, {
-      ariaLabelledBy: 'modal-basic-title',
-      size: 'xl',
-      centered: true,
-     windowClass: 'border-custom-popup'
-
-    }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
 }
