c70c6a95e0935e6b9767a2d45158b81b3b43e711
Camilo H <camilohamonserna@gmail.com>
2021-07-29 23:37:40 -0500

    Parcial finish backend

--- backend/resources/views/emails/message_received.blade.php
+++ backend/resources/views/emails/message_received.blade.php
@@ -9,7 +9,7 @@
         <p>Se ha creado tu usuario satisfactoriamente con los siguiente datos de acceso:</p>
         <p><strong>Correo: {{$email}}</strong></p>
         <p><strong>Password: {{$random_password}}</strong></p>
-        <a href="http://localhost:4200">Ingrese aquí para iniciar sesión y cambiar su contraseña.</a>
+        <a href="http://localhost:4200">Ingresa aquí para iniciar sesión y cambiar tu contraseña.</a>
     </div>
 </body>
 
